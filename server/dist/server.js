6095250604d7819ea2640773b6d9095d5ab0d933
SitaGomes <ansitagomes@gmail.com>
2022-09-18 20:27:34 -0300

    feat: prisma init

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,6 +1,22 @@
-import express from "express";
-const app = express();
-app.get("/", (request, response) => {
-    response.json({ message: "Dist directory babyyyy" });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/games", (request, response) => {
+    return response.status(200).json([]);
+});
+app.post("/ads", (request, response) => {
+    return response.status(201).json([]);
+});
+app.get("/games/:id/ads", (request, response) => {
+    const gameId = request.params.id;
+    return response.status(200).json([{ message: "Dist directory babyyyy" }, { message: "Ts-node-dev" }]);
+});
+app.get("/ads/:id/discord", (request, response) => {
+    const adsId = request.params.id;
+    return response.status(200).json(adsId);
 });
 app.listen(3333);
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,28 @@ import express, { Request, Response } from "express"
 
 const app = express()
 
-app.get("/", (request: Request, response: Response) => {
-    response.json([{message: "Dist directory babyyyy"}, {message: "Ts-node-dev"}])
+app.post("/ads", (request: Request, response: Response) => {
+    return response.status(201).json([])
 })
+
+
+app.get("/games", (request: Request, response: Response) => {
+    return response.status(200).send("Ola mundo")
+})
+
+
+app.get("/games/:id/ads", (request: Request, response: Response) => {
+    const gameId = request.params.id
+
+    return response.status(200).json(gameId)
+})
+
+
+app.get("/ads/:id/discord", (request: Request, response: Response) => {
+    const adsId = request.params.id
+
+    return response.status(200).json(adsId)
+})
+
 
 app.listen(3333)
